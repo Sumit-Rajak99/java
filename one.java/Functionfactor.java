ae895e4757d30b753ab46d14754ec8b2c44ebaf5
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-12-15 10:26:14 +0530

    add

--- one.java/Functionfactor.java
+++ one.java/Functionfactor.java
@@ -6,6 +6,7 @@ class Functionfactor{
         for (int i = 1; i <= num; i++) {
            if(num%i==0){
          System.out.println(i);
+        ans=ans+i;
            }
         }
         return ans;
